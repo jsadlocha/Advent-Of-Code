afe22eb8ceb62a19d6813b3948a253070a9cb412
jsadlocha <jr.sadlocha@gmail.com>
2023-12-04 09:49:39 +0100

    Delete unnecessary files

--- Day1/Kernel_windows/Advent/advent.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <ntifs.h>
-#include <ntddk.h>
-
-extern "C" NTSTATUS DriverEntry(PDRIVER_OBJECT pDriverObject, PUNICODE_STRING pRegisterPath);
-
-void DriverUnload(PDRIVER_OBJECT pDriverObject);
